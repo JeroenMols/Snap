efd7ef409c995f6b26168c7bda726c2254095785
Jeroen Mols <jmols.mobile@gmail.com>
2018-11-24 14:36:38 +0100

    Renamed MainActivityFragment to PhotosFragment

--- app/src/main/java/com/jeroenmols/snap/PhotosFragment.kt
+++ app/src/main/java/com/jeroenmols/snap/PhotosFragment.kt
@@ -9,12 +9,12 @@ import androidx.fragment.app.Fragment
 /**
  * A placeholder fragment containing a simple view.
  */
-class MainActivityFragment : Fragment() {
+class PhotosFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_photos, container, false)
     }
 }
