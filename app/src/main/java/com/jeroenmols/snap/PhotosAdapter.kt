5cba79ca87c43fceb923fdc368ac18b4d8f212d1
Jeroen Mols <jmols.mobile@gmail.com>
2018-11-25 14:50:07 +0100

    Basic UI to display photos

---
+++ app/src/main/java/com/jeroenmols/snap/PhotosAdapter.kt
@@ -0,0 +1,39 @@
+package com.jeroenmols.snap
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jeroenmols.snap.unsplash.data.UnsplashPhoto
+import com.squareup.picasso.Picasso
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_photo.*
+
+class PhotosAdapter : RecyclerView.Adapter<PhotosAdapter.PhotosViewHolder>() {
+
+    var photos: List<UnsplashPhoto> = emptyList()
+        set(value) {
+            notifyDataSetChanged()
+            field = value
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotosViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_photo, parent, false)
+        return PhotosViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = photos.size
+
+    override fun onBindViewHolder(holder: PhotosViewHolder, position: Int) {
+        holder.bind(photos[position])
+    }
+
+    class PhotosViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+        LayoutContainer {
+
+        fun bind(photo: UnsplashPhoto) {
+            Picasso.get().load(photo.urls["regular"]).placeholder(R.drawable.photo_placeholder).into(imageview_photo)
+        }
+    }
+}
--- app/src/main/java/com/jeroenmols/snap/PhotosFragment.kt
+++ app/src/main/java/com/jeroenmols/snap/PhotosFragment.kt
@@ -1,37 +1,37 @@
 package com.jeroenmols.snap
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jeroenmols.snap.unsplash.data.UnsplashPhoto
+import kotlinx.android.synthetic.main.fragment_photos.*
 
-/**
- * A placeholder fragment containing a simple view.
- */
 class PhotosFragment : Fragment() {
 
     lateinit var viewModel: PhotosViewModel
+    lateinit var adapter: PhotosAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProviders.of(this, PhotosViewModelFactory()).get(PhotosViewModel::class.java)
+        adapter = PhotosAdapter()
 
-        viewModel.photos.observe(this, object : Observer<List<UnsplashPhoto>> {
-            override fun onChanged(t: List<UnsplashPhoto>?) {
-                t?.forEach{ Log.v("JEROEN", "Loaded image: ${it.id} with url: ${it.urls["raw"]}") }
-            }
-        })
+        viewModel.photos.observe(this, Observer<List<UnsplashPhoto>> { photos -> adapter.photos = photos })
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_photos, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        photos_recyclerview.adapter = adapter
+        photos_recyclerview.layoutManager = LinearLayoutManager(activity)
+    }
+
 }
