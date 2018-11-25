55cb9b07d6d957037fd831cb3d3a29f92c8c19ff
Jeroen Mols <jmols.mobile@gmail.com>
2018-11-25 12:49:58 +0100

    Factory should create ViewModel

--- app/src/main/java/com/jeroenmols/snap/PhotosViewModelFactory.kt
+++ app/src/main/java/com/jeroenmols/snap/PhotosViewModelFactory.kt
@@ -1,5 +1,19 @@
 package com.jeroenmols.snap
 
-class PhotosViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.jeroenmols.snap.unsplash.WebService
+import java.lang.RuntimeException
+
+class PhotosViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PhotosViewModel::class.java)) {
+            return PhotosViewModel(WebService()) as T
+        } else {
+            throw RuntimeException()
+        }
+    }
 
 }
--- app/src/test/java/com/jeroenmols/snap/PhotosViewModelFactoryTest.kt
+++ app/src/test/java/com/jeroenmols/snap/PhotosViewModelFactoryTest.kt
@@ -1,5 +1,6 @@
 package com.jeroenmols.snap
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class PhotosViewModelFactoryTest {
@@ -7,5 +8,12 @@ class PhotosViewModelFactoryTest {
     @Test
     internal fun `can instantiate`() {
         PhotosViewModelFactory()
+    }
+
+    @Test
+    internal fun `should create ViewModel for PhotosViewModelClass`() {
+        val viewModel = PhotosViewModelFactory().create(PhotosViewModel::class.java)
+
+        assertThat(viewModel).isNotNull
     }
 }
