9e51ce0bf71cf54c03d6665404f661e29f063524
Jeroen Mols <jmols.mobile@gmail.com>
2018-11-25 13:56:33 +0100

    Hook ViewModel up to PhotosFragment

--- app/src/main/java/com/jeroenmols/snap/MainActivity.kt
+++ app/src/main/java/com/jeroenmols/snap/MainActivity.kt
@@ -1,19 +1,8 @@
 package com.jeroenmols.snap
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
-import com.jeroenmols.snap.unsplash.WebService
-import com.jeroenmols.snap.unsplash.api.UnsplashService
-import com.jeroenmols.snap.unsplash.data.UnsplashPhoto
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,25 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
-        WebService().getPhotos(MyCallback())
-    }
-
-    class MyCallback : Callback<List<UnsplashPhoto>> {
-        override fun onResponse(call: Call<List<UnsplashPhoto>>, response: Response<List<UnsplashPhoto>>) {
-            val photos = response.body()!!
-            photos.forEach { Log.v("JEROEN", "Loaded image: ${it.id} with url: ${it.urls["raw"]}") }
-        }
-
-        override fun onFailure(call: Call<List<UnsplashPhoto>>, t: Throwable) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
     }
 }
 
--- app/src/main/java/com/jeroenmols/snap/PhotosFragment.kt
+++ app/src/main/java/com/jeroenmols/snap/PhotosFragment.kt
@@ -1,15 +1,32 @@
 package com.jeroenmols.snap
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.jeroenmols.snap.unsplash.data.UnsplashPhoto
 
 /**
  * A placeholder fragment containing a simple view.
  */
 class PhotosFragment : Fragment() {
+
+    lateinit var viewModel: PhotosViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this, PhotosViewModelFactory()).get(PhotosViewModel::class.java)
+
+        viewModel.photos.observe(this, object : Observer<List<UnsplashPhoto>> {
+            override fun onChanged(t: List<UnsplashPhoto>?) {
+                t?.forEach{ Log.v("JEROEN", "Loaded image: ${it.id} with url: ${it.urls["raw"]}") }
+            }
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
