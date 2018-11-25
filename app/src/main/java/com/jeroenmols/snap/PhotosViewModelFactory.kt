b8d65a8e3e6636d9c188ba975a7defe97cd4cbfa
Jeroen Mols <jmols.mobile@gmail.com>
2018-11-25 12:38:42 +0100

    Added PhotosViewModelFactory

---
+++ app/src/main/java/com/jeroenmols/snap/PhotosViewModelFactory.kt
@@ -0,0 +1,5 @@
+package com.jeroenmols.snap
+
+class PhotosViewModelFactory {
+
+}
---
+++ app/src/test/java/com/jeroenmols/snap/PhotosViewModelFactoryTest.kt
@@ -0,0 +1,11 @@
+package com.jeroenmols.snap
+
+import org.junit.jupiter.api.Test
+
+class PhotosViewModelFactoryTest {
+
+    @Test
+    internal fun `can instantiate`() {
+        PhotosViewModelFactory()
+    }
+}
