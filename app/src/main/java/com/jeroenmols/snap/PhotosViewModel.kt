e4ee003844cdfdf668a9e8919c34811f464bfc32
Jeroen Mols <jmols.mobile@gmail.com>
2018-11-25 12:36:00 +0100

    Load photos while creating ViewModel and update Livedata

--- app/src/main/java/com/jeroenmols/snap/PhotosViewModel.kt
+++ app/src/main/java/com/jeroenmols/snap/PhotosViewModel.kt
@@ -1,7 +1,27 @@
 package com.jeroenmols.snap
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jeroenmols.snap.unsplash.WebService
+import com.jeroenmols.snap.unsplash.data.UnsplashPhoto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class PhotosViewModel : ViewModel() {
+class PhotosViewModel(webService: WebService) : ViewModel() {
 
+    val photos = MutableLiveData<List<UnsplashPhoto>>()
+
+    init {
+        webService.getPhotos(object : Callback<List<UnsplashPhoto>> {
+            override fun onResponse(call: Call<List<UnsplashPhoto>>, response: Response<List<UnsplashPhoto>>) {
+                photos.postValue(response.body())
+            }
+
+            override fun onFailure(call: Call<List<UnsplashPhoto>>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+    }
 }
--- app/src/test/java/com/jeroenmols/snap/PhotosViewModelTest.kt
+++ app/src/test/java/com/jeroenmols/snap/PhotosViewModelTest.kt
@@ -1,11 +1,54 @@
 package com.jeroenmols.snap
 
+import com.jeroenmols.snap.testutils.InstantExecutorExtension
+import com.jeroenmols.snap.unsplash.WebService
+import com.jeroenmols.snap.unsplash.data.UnsplashPhoto
+import com.nhaarman.mockitokotlin2.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import retrofit2.Callback
+import retrofit2.Response
 
+@ExtendWith(MockitoExtension::class, InstantExecutorExtension::class)
 class PhotosViewModelTest {
+
+    @Mock
+    lateinit var webService: WebService
+
+    val captor = argumentCaptor<Callback<List<UnsplashPhoto>>>()
 
     @Test
     internal fun `can instantiate`() {
-        PhotosViewModel()
+        PhotosViewModel(webService)
     }
+
+    @Test
+    internal fun `should load photos when creating`() {
+        PhotosViewModel(webService)
+
+        verify(webService).getPhotos(any())
+    }
+
+    @Test
+    internal fun `should update live data with photos`() {
+        val photos = fakePhotoList()
+        val viewModel = PhotosViewModel(webService)
+        verify(webService).getPhotos(captor.capture())
+
+        captor.firstValue.onResponse(mock(), photos.asResponse())
+
+        assertThat(viewModel.photos.value).isEqualTo(photos)
+    }
+
+    private fun List<UnsplashPhoto>.asResponse(): Response<List<UnsplashPhoto>> {
+        val response: Response<List<UnsplashPhoto>> = mock()
+        whenever(response.body()).thenReturn(this)
+        return response
+    }
+
+    private fun fakePhotoList() = listOf(UnsplashPhoto("id1", "", emptyMap()))
 }
+
