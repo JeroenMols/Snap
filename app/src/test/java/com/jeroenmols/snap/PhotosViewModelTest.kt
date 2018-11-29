1185cf1d767241138bc466d58bf8e97f008814b1
Jeroen Mols <jmols.mobile@gmail.com>
2018-11-29 12:56:39 +0100

    Converted PhotosViewModelTest to RXJava

--- app/src/test/java/com/jeroenmols/snap/PhotosViewModelTest.kt
+++ app/src/test/java/com/jeroenmols/snap/PhotosViewModelTest.kt
@@ -1,81 +1,70 @@
 package com.jeroenmols.snap
 
+import com.jeroenmols.snap.testutils.ImmediateRxSchedulersExtension
 import com.jeroenmols.snap.testutils.InstantExecutorExtension
 import com.jeroenmols.snap.unsplash.WebService
-import com.jeroenmols.snap.unsplash.data.SearchResult
 import com.jeroenmols.snap.unsplash.data.UnsplashPhoto
-import com.nhaarman.mockitokotlin2.*
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Single
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
-import retrofit2.Callback
-import retrofit2.Response
 
-@ExtendWith(MockitoExtension::class, InstantExecutorExtension::class)
+@ExtendWith(MockitoExtension::class, InstantExecutorExtension::class, ImmediateRxSchedulersExtension::class)
 class PhotosViewModelTest {
 
     @Mock
     lateinit var webService: WebService
 
-    val photosCaptor = argumentCaptor<Callback<List<UnsplashPhoto>>>()
-    val searchCaptor = argumentCaptor<Callback<SearchResult>>()
-
     @Test
     internal fun `can instantiate`() {
+        whenever(webService.getPhotos()).thenReturn(Single.just(fakePhotoList()))
         PhotosViewModel(webService)
     }
 
     @Test
     internal fun `should load photos when creating`() {
+        whenever(webService.getPhotos()).thenReturn(Single.just(fakePhotoList()))
         PhotosViewModel(webService)
 
-        verify(webService).getPhotos(any())
+        verify(webService).getPhotos()
     }
 
     @Test
     internal fun `should update live data with photos`() {
         val photos = fakePhotoList()
-        val viewModel = PhotosViewModel(webService)
-        verify(webService).getPhotos(photosCaptor.capture())
+        whenever(webService.getPhotos()).thenReturn(Single.just(photos))
 
-        photosCaptor.firstValue.onResponse(mock(), photos.asResponse())
+        val viewModel = PhotosViewModel(webService)
 
         assertThat(viewModel.photos.value).isEqualTo(photos)
     }
 
     @Test
     internal fun `should search photos with webservice when search`() {
+        whenever(webService.getPhotos()).thenReturn(Single.just(fakePhotoList()))
+        whenever(webService.searchPhotos("lego")).thenReturn(Single.just(fakePhotoList()))
+
         PhotosViewModel(webService).search("lego")
 
-        verify(webService).searchPhotos(eq("lego"), any())
+        verify(webService).searchPhotos(eq("lego"))
     }
 
     @Test
     internal fun `should update live data with photos from search`() {
         val photos = fakePhotoList()
+        whenever(webService.getPhotos()).thenReturn(Single.just(fakePhotoList()))
+        whenever(webService.searchPhotos("lego")).thenReturn(Single.just(photos))
         val viewModel = PhotosViewModel(webService)
-        viewModel.search("lego")
-        verify(webService).searchPhotos(eq("lego"), searchCaptor.capture())
 
-        searchCaptor.firstValue.onResponse(mock(), photos.asSearchResponse())
+        viewModel.search("lego")
 
         assertThat(viewModel.photos.value).isEqualTo(photos)
     }
 
-    private fun List<UnsplashPhoto>.asResponse(): Response<List<UnsplashPhoto>> {
-        val response: Response<List<UnsplashPhoto>> = mock()
-        whenever(response.body()).thenReturn(this)
-        return response
-    }
-
-    private fun List<UnsplashPhoto>.asSearchResponse(): Response<SearchResult> {
-        val response: Response<SearchResult> = mock()
-        whenever(response.body()).thenReturn(SearchResult(this))
-        return response
-    }
-
     private fun fakePhotoList() = listOf(UnsplashPhoto("id1", "", emptyMap()))
 }
-
