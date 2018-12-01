ac7615c8f4ece4532dc3bd6fbabd033efb68822a
Jeroen Mols <jmols.mobile@gmail.com>
2018-12-01 14:15:56 +0100

    Enabled logging on WebService

--- app/src/main/java/com/jeroenmols/snap/unsplash/WebService.kt
+++ app/src/main/java/com/jeroenmols/snap/unsplash/WebService.kt
@@ -2,11 +2,11 @@ package com.jeroenmols.snap.unsplash
 
 import android.util.Log
 import com.jeroenmols.snap.unsplash.api.UnsplashService
-import com.jeroenmols.snap.unsplash.data.SearchResult
 import com.jeroenmols.snap.unsplash.data.UnsplashPhoto
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
-import retrofit2.Callback
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -16,7 +16,12 @@ class WebService {
     private val unsplashService: UnsplashService
 
     init {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
         val retrofit = Retrofit.Builder()
+            .client(client)
             .baseUrl("https://api.unsplash.com")
             .addConverterFactory(MoshiConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
@@ -26,13 +31,8 @@ class WebService {
 
     }
 
-    fun getPhotos() : Single<List<UnsplashPhoto>> {
-        Log.v("JEROEN", "Doing a network call")
-        return unsplashService.getPhotos()
-    }
+    fun getPhotos(): Single<List<UnsplashPhoto>> = unsplashService.getPhotos()
 
-    fun searchPhotos(searchTerm : String) : Single<List<UnsplashPhoto>> {
-        Log.v("JEROEN", "Doing a network call")
-        return unsplashService.searchPhotos(searchTerm).map { it.results }
-    }
+    fun searchPhotos(searchTerm: String): Single<List<UnsplashPhoto>> =
+        unsplashService.searchPhotos(searchTerm).map { it.results }
 }
