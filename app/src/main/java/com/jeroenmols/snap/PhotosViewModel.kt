33d15ff2bcee6b447f6145e68c13ef1ab0880eb3
Jeroen Mols <jmols.mobile@gmail.com>
2018-12-01 10:42:16 +0100

    complete and utter hack of ignoring previous search results

--- app/src/main/java/com/jeroenmols/snap/PhotosViewModel.kt
+++ app/src/main/java/com/jeroenmols/snap/PhotosViewModel.kt
@@ -4,16 +4,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.jeroenmols.snap.unsplash.WebService
 import com.jeroenmols.snap.unsplash.data.UnsplashPhoto
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.TestObserver
 
 class PhotosViewModel(private val webService: WebService) : ViewModel() {
 
     val photos = MutableLiveData<List<UnsplashPhoto>>()
+    var disposable :Disposable = TestObserver<Any>()
 
     init {
         webService.getPhotos().subscribe(photos::postValue)
     }
 
     fun search(query: String) {
-        webService.searchPhotos(query).subscribe(photos::postValue)
+        disposable.dispose()
+        disposable = webService.searchPhotos(query).subscribe(photos::postValue)
     }
 }
